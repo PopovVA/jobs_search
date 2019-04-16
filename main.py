0ce67b49a6350629d1b42b9fc2d3e46e0e097cff
Vadim Popov <m.vadimpopov@gmail.com>
2019-04-16 14:38:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from terminaltables import AsciiTable, DoubleTable, SingleTable
 
 def main():
 
-  TITLE = 'HeadHunter Moscow'
+  title = 'HeadHunter Moscow'
 
-  TABLE_DATA = (
+  table_data = (
     ('Development languages', 'Amount vacancies', 'Average salary'),
     ('python', get_amount_vacancies_for_hh('Программист python') ,predict_rub_salary_for_hh('Программист python')),
     ('Java', get_amount_vacancies_for_hh('Программист Java') ,predict_rub_salary_for_hh('Программист Java')),
@@ -17,11 +17,11 @@ def main():
     ('Java script', get_amount_vacancies_for_hh('Программист Java script') ,predict_rub_salary_for_hh('Программист Java script')),
   )
 
-  display_table(TABLE_DATA,TITLE)
+  display_table(table_data,title)
 
-  TITLE = 'SuperJob Moscow'
+  title = 'SuperJob Moscow'
 
-  TABLE_DATA = (
+  table_data = (
     ('Development languages', 'Amount vacancies', 'Average salary'),
     ('python', get_amount_vacancies_for_sj('Программист python') ,predict_rub_salary_for_sj('Программист python')),
     ('Java', get_amount_vacancies_for_sj('Программист Java') ,predict_rub_salary_for_sj('Программист Java')),
@@ -30,21 +30,20 @@ def main():
     ('Java script', get_amount_vacancies_for_sj('Программист Java script') ,predict_rub_salary_for_sj('Программист Java script')),
   )
 
-  display_table(TABLE_DATA,TITLE)
-  
+  display_table(table_data,title)
 
 
-def display_table(TABLE_DATA,title):
-  table_instance = DoubleTable(TABLE_DATA, title)
+def display_table(table_data,title):
+  table_instance = DoubleTable(table_data, title)
   table_instance.justify_columns[2] = 'right'
   print(table_instance.table)
   print()
 
 
-def get_predict_salary(salary_from, salary_to):
-  if not salary_from : 
+def predict_salary(salary_from, salary_to):
+  if not salary_from: 
     return salary_to * 0.8
-  if not salary_to  :
+  if not salary_to:
     return salary_from * 1.2
   average_salary = statistics.mean([salary_from,salary_to])
   return average_salary
@@ -63,64 +62,87 @@ def get_amount_vacancies_for_sj(vacancy):
   }
   base_url = 'https://api.superjob.ru/2.0/vacancies/'
   response = requests.get(base_url, headers=headers, params=params)
-  return(response.json()['total'])
+  return response.json()['total']
 
-def predict_rub_salary_for_sj(vacancy):
-  token = os.getenv("SecretKey")
-  headers = {
-    'X-Api-App-Id': token
-  }
-  params = {
-    'not_archive' : '1',
-    't': '4',
-    'catalogues': '48',
-    'no_agreement': '1',
-    'keyword': vacancy
-  }
-  base_url = 'https://api.superjob.ru/2.0/vacancies/'
-  response = requests.get(base_url, headers=headers, params=params)
+def predict_rub_salary_for_sj(vacancy_name):
+  page = pages = 0
+  total_salary_list = [] 
+  while page <= pages:
+    token = os.getenv("SecretKey")
+    headers = {
+      'X-Api-App-Id': token
+    }
+    params = {
+      'not_archive' : '1',
+      't': '4',
+      'page': page,
+      'catalogues': '48',
+      'no_agreement': '1',
+      'keyword': vacancy_name
+    }
+    base_url = 'https://api.superjob.ru/2.0/vacancies/'
+    response = requests.get(base_url, headers=headers, params=params)
 
-  salary_list = []
-  for vacancy in response.json()['objects']: 
-    average_salary = get_predict_salary(vacancy['payment_from'],vacancy['payment_to']) 
-    salary_list.append(average_salary)
+    salary_list = []
+    for vacancy in response.json()['objects']: 
+      average_salary = predict_salary(vacancy['payment_from'],vacancy['payment_to']) 
+      salary_list.append(average_salary)
 
-  if not salary_list:
-    return 0
-  sorted_salary = sorted(salary_list)
-  min_salary = sorted_salary[0]
-  max_salary = sorted_salary[-1]
-  total_average_salary = get_predict_salary(min_salary,max_salary)
-  return total_average_salary
+    if not salary_list:
+      return 0
+    sorted_salary = sorted(salary_list)
+    min_salary = sorted_salary[0]
+    max_salary = sorted_salary[-1]
+    total_average_salary = predict_salary(min_salary,max_salary)
+    total_salary_list.appen(total_average_salary)
 
-def predict_rub_salary_for_hh(vacancy):
-  headers = {
-      'User-Agent': 'SearchingJob/1.0 (m.vadimpopov@gmail.com)'
-  }
-  params = {
-    'text' : vacancy,
-    'area' : '1',
-    'period' : '30',
-    'page' : '1',
-    'only_with_salary' : 'true',
-    'per_page': '100'
-  }
-  base_url = 'https://api.hh.ru/vacancies'
-  response = requests.get(base_url, headers=headers, params=params)
-  salary_list = []
-  for vacancy in response.json()['items']:
-    salary = vacancy['salary']
-    average_salary = get_predict_salary(salary['from'],salary['to'])
-    salary_list.append(average_salary)
+  total_sorted_salary = sorted(total_salary_list)
+  total_min_salary = total_sorted_salary[0]
+  total_max_salary = total_sorted_salary[-1]
+  return predict_salary(total_min_salary,total_max_salary)
 
-  if not salary_list:
-    return 0
+def predict_rub_salary_for_hh(vacancy_name):
+  page = pages = 0
+  total_salary_list = [] 
+  while page <= pages:
+    headers = {
+        'User-Agent': 'SearchingJob/1.0 (m.vadimpopov@gmail.com)'
+    }   
+    params = {
+      'text' : vacancy_name,
+      'area' : '1',
+      'period' : '30',
+      'page' : page,
+      'only_with_salary' : 'true',
+      'per_page': '100',
+      'no_magic': True,
+      'premium' : False
+    }
+    base_url = 'https://api.hh.ru/vacancies'
+    response = requests.get(base_url, headers=headers, params=params)
+    response = response.json()
+    pages = response['pages']
+    salary_list = []
+    for vacancy in response['items']:      
+      salary = vacancy['salary']
+      average_salary = predict_salary(salary['from'],salary['to'])
+      salary_list.append(average_salary)
     
-  sorted_salary = sorted(salary_list)
-  min_salary = sorted_salary[0]
-  max_salary = sorted_salary[-1]
-  total_average_salary = get_predict_salary(min_salary,max_salary)
-  return total_average_salary
+    if not salary_list:
+      page += 1
+      continue
+
+    sorted_salary = sorted(salary_list)
+    min_salary = sorted_salary[0]
+    max_salary = sorted_salary[-1]
+    total_average_salary = predict_salary(min_salary,max_salary)
+    total_salary_list.append(total_average_salary)
+    page += 1
+
+  total_sorted_salary = sorted(total_salary_list)
+  total_min_salary = total_sorted_salary[0]
+  total_max_salary = total_sorted_salary[-1]
+  return predict_salary(total_min_salary,total_max_salary)
     
 
 def get_amount_vacancies_for_hh(vacancy):
